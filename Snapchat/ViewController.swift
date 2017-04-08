77e875704b5399f3bac11b3d318156d8d179072b
ANTHONY MOREE <moree115@ANTHONY-MOREEs-iMac.local>
2017-04-08 15:06:08 -0400

    Initial Commit

---
+++ Snapchat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snapchat
+//
+//  Created by ANTHONY MOREE on 4/8/17.
+//  Copyright © 2017 ANTHONY MOREE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
